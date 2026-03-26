fa559562f185021f19c2752d75fcc3bff4eb8458
AlyszaZealand <als012@edu.zealand.dk>
2026-03-26 15:35:53 +0100

    commit

--- src/test/java/feedback/pizzaparadise_pizzabestillingssystem/model/UserTest.java
+++ src/test/java/feedback/pizzaparadise_pizzabestillingssystem/model/UserTest.java
@@ -23,7 +23,7 @@ class UserTest {
         // Test Email
         assertEquals("Bob@gmail.com", user.getEmail());
 
-        // Test Addresse
+        // Test Adresse
         assertEquals("Bobbystreet69", user.getAddress());
 
         // Test Bonuspoints
@@ -32,8 +32,56 @@ class UserTest {
 
     @Test
     void testConstructorWithoutID() {
+        User user = new User("Keld",
+                "Keld@gmail.com",
+                "Keldstreet420",
+                2);
+
+        // Test ID = null
+        assertEquals(0, user.getUserID());
+
+        // Test Navn
+        assertEquals("Keld", user.getName());
+
+        // Test Adresse
+        assertEquals("Keld@gmail.com", user.getEmail());
+
+        // Test Email
+        assertEquals("Keldstreet420", user.getAddress());
+
+        // Test Bonuspoints
+        assertEquals(2, user.getBonuspoints());
     }
 
+    @Test
+    void testEmptyConstructor() {
+        User user = new User();
 
+        // Test ID = null
+        assertEquals(0, user.getUserID());
+
+        // Test Navn = Null ( kunne ikke "")
+        assertEquals(null, user.getName());
+
+        // Test Bonuspoints = null
+        assertEquals(0, user.getBonuspoints());
+    }
+
+    @Test
+    void testSetters() {
+        User user = new User();
+
+        // Set Navn
+        user.setName("GIGACHAD");
+
+        // Test Set Navn
+        assertEquals("GIGACHAD", user.getName());
+
+        // Set Bonuspoints
+        user.setBonuspoints(10);
+
+        // Test Set Bonuspoints
+        assertEquals(10, user.getBonuspoints());
+    }
 
 }
---
+++ src/test/java/feedback/pizzaparadise_pizzabestillingssystem/model/repositories/OrdreRepositoryTest.java
@@ -0,0 +1,7 @@
+package feedback.pizzaparadise_pizzabestillingssystem.model.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdreRepositoryTest {
+
+}
